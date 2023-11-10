b0dbca18ba3ee1adb7897049b6e0ce3a6ae92eb3
Tóth Róbert <tothr94@gmail.com>
2023-11-11 00:51:41 +0100

    Fix AggregationCountryQueries

--- src/main/java/base/AggregationCountryQueries.java
+++ src/main/java/base/AggregationCountryQueries.java
@@ -15,140 +15,69 @@ public interface AggregationCountryQueries {
     /**
      * Returns a dictionary which maps each country code to the corresponding country.
      *
-     * @param countries the list of countries
      * @return the dictionary
      */
-    Map<String, Country> getCountriesByCodes(
-            List<Country> countries);
+    Map<String, Country> getCountriesByCodes();
 
     /**
      * Returns a dictionary which maps each region to the count of its countries.
      *
-     * @param countries the list of countries
      * @return the dictionary
      */
-    Map<Country.Region, Long> getCountOfCountriesByRegions(
-            List<Country> countries);
+    Map<Country.Region, Long> getCountOfCountriesByRegions();
 
     /**
      * Returns a dictionary which maps each region to its countries.
      *
-     * @param countries the list of countries
      * @return the dictionary
      */
-    Map<Country.Region, Set<Country>> getCountriesByRegions(
-            List<Country> countries);
+    Map<Country.Region, Set<Country>> getCountriesByRegions();
 
     /**
      * Returns a dictionary which maps each region to its most populous country.
      *
-     * @param countries the list of countries
      * @return the dictionary
      */
-    Map<Country.Region, Optional<Country>> getMostPopulousCountryByRegions(
-            List<Country> countries);
+    Map<Country.Region, Optional<Country>> getMostPopulousCountryByRegions();
 
     /**
      * Returns a dictionary which maps each region the list of its countries ordered by their capital names to their continent.
      *
-     * @param countries the list of countries
      * @return the dictionary
      */
-    Map<Country.Region, List<Country>> getCountriesByRegionsOrderByCapitals(
-            List<Country> countries);
+    Map<Country.Region, List<Country>> getCountriesByRegionsOrderByCapitals();
 
     /**
      * Returns a dictionary which maps each region to the corresponding countries which population is between the given bounds (inclusive) to their continent.
      *
-     * @param countries  the list of countries
      * @param lowerBound the lower bound
      * @param upperBound the upper bound
      * @return the dictionary
      */
     Map<Country.Region, Set<Country>> getCountriesByRegionsFilterByPopulation(
-            List<Country> countries,
             long lowerBound,
             long upperBound);
 
     /**
      * Returns a dictionary which maps each region to the corresponding country codes, then each country code to the corresponding country.
      *
-     * @param countries the list of countries
      * @return the dictionary
      */
-    Map<Country.Region, Map<String, Country>> getCountriesByRegionsAndCodes(
-            List<Country> countries);
+    Map<Country.Region, Map<String, Country>> getCountriesByRegionsAndCodes();
 
     /**
      * Returns a dictionary which maps region to the letters that occur in the first place of corresponding
      * country names, then each letter to the corresponding countries.
      *
-     * @param countries the list of countries
      * @return the dictionary
      */
-    Map<Country.Region, Map<String, Set<Country>>> getCountriesByRegionsAndFirstLetters(
-            List<Country> countries);
+    Map<Country.Region, Map<String, Set<Country>>> getCountriesByRegionsAndFirstLetters();
 
     /**
      * Returns a dictionary which maps the letters that occur in the first place of corresponding
      * country names to regions, then each letter to the corresponding countries.
      *
-     * @param countries the list of countries
      * @return the dictionary
      */
-    Map<String, Map<Country.Region, Set<Country>>> getCountriesByFirstLettersAndRegions(
-            List<Country> countries);
-
-    /**
-     * Returns a dictionary which maps each region to the set of country names, using the given locale.
-     *
-     * @param countries the list of countries
-     * @param locale    the locale
-     * @return the dictionary
-     */
-    Map<Country.Region, Set<String>> getLocalizedCountryNamesByRegions(
-            List<Country> countries,
-            String locale);
-
-    /**
-     * Returns a dictionary which maps each locale to the corresponding country names.
-     *
-     * @param countries the list of countries
-     * @return the dictionary
-     */
-    Map<String, Set<String>> getCountryNamesByLocales(
-            List<Country> countries);
-
-    /**
-     * Returns a dictionary which maps each locale to the corresponding country names which countries are located in the given region.
-     *
-     * @param countries the list of countries
-     * @param region    the region
-     * @return the dictionary
-     */
-    Map<String, Set<String>> getCountryNamesByLocales(
-            List<Country> countries,
-            Country.Region region);
-
-    /**
-     * Returns a dictionary which maps each region to the country name which is the first in the given locale.
-     *
-     * @param countries the list of countries
-     * @param locale    the locale
-     * @return the dictionary
-     */
-    Map<Country.Region, Optional<String>> getFirstLocalizedCountryNamesByRegions(
-            List<Country> countries,
-            String locale);
-
-    /**
-     * Returns a dictionary which maps each region to the country which name is the first in the given locale.
-     *
-     * @param countries the list of countries
-     * @param locale    the locale
-     * @return the dictionary
-     */
-    Map<Country.Region, Optional<Country>> getFirstLocalizedCountriesByRegions(
-            List<Country> countries,
-            String locale);
+    Map<String, Map<Country.Region, Set<Country>>> getCountriesByFirstLettersAndRegions();
 }
